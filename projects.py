8a66af502be112f49fbb64f68936454f44d11946
Eugene Antonov <jetman@sysadm.ws>
2020-07-17 08:37:42 +0300

    many new options added

--- projects.py
+++ projects.py
@@ -7,6 +7,7 @@ import gitlab
 import glob
 import textwrap
 import subprocess
+import json
 
 # Constants and envs
 
@@ -89,6 +90,7 @@ if __name__ == "__main__":
                         group_full_path = "/".join(project_dict["path"].split("/")[:-1])
                         # Search groups by last name before project and match full path
                         group_name = project_dict["path"].split("/")[-2]
+                        group_id = None
                         for group in gl.groups.list(search=group_name):
                             if group.full_path == group_full_path:
                                 group_id = group.id
@@ -129,11 +131,16 @@ if __name__ == "__main__":
                             project.packages_enabled = project_dict["packages_enabled"]
                         if "approvals_before_merge" in project_dict:
                             project.approvals_before_merge = project_dict["approvals_before_merge"]
+                        if "service_desk_enabled" in project_dict:
+                            project.service_desk_enabled = project_dict["service_desk_enabled"]
+                        # Todo: project.forward_deployment_enabled
+                        # https://gitlab.com/gitlab-org/gitlab/-/issues/212621
                         # Maintainer group
                         if "maintainers_group" in project_dict:
                             # Get maintainers_group_id by maintainers_group
                             # Search groups by last name before project and match full path
                             group_name = project_dict["maintainers_group"].split("/")[-1]
+                            group_id = None
                             for group in gl.groups.list(search=group_name):
                                 if group.full_path == project_dict["maintainers_group"]:
                                     group_id = group.id
@@ -144,11 +151,129 @@ if __name__ == "__main__":
                         if "deploy_keys" in project_dict:
                             for deploy_key in project_dict["deploy_keys"]:
                                 key = project.keys.create({'title': deploy_key["title"], 'key': deploy_key["key"]})
+                        # Deploy tokens
+                        if "deploy_tokens" in project_dict:
+                            for deploy_token in project_dict["deploy_tokens"]:
+                                # Tokens should be explicitly removed each time as revoked manually with the same name exist forever and cannot be detected revoked
+                                for token in project.deploytokens.list():
+                                    if token.name == deploy_token["name"]:
+                                        token.delete()
+                                token = project.deploytokens.create({'name': deploy_token["name"], 'scopes': deploy_token["scopes"]})
+                                logger.info("Project {project} deploy token added:".format(project=project_dict["path"]))
+                                logger.info(token)
+                        # MR approval rules
+                        if "approvals_before_merge" in project_dict:
+                            p_mras = project.approvals.get()
+                            p_mras.approvals_before_merge = project_dict["approvals_before_merge"]
+                            p_mras.save()
+                        if "reset_approvals_on_push" in project_dict:
+                            p_mras = project.approvals.get()
+                            p_mras.reset_approvals_on_push = project_dict["reset_approvals_on_push"]
+                            p_mras.save()
+                        if "disable_overriding_approvers_per_merge_request" in project_dict:
+                            p_mras = project.approvals.get()
+                            p_mras.disable_overriding_approvers_per_merge_request = project_dict["disable_overriding_approvers_per_merge_request"]
+                            p_mras.save()
+                        if "merge_requests_author_approval" in project_dict:
+                            p_mras = project.approvals.get()
+                            p_mras.merge_requests_author_approval = project_dict["merge_requests_author_approval"]
+                            p_mras.save()
+                        if "merge_requests_disable_committers_approval" in project_dict:
+                            p_mras = project.approvals.get()
+                            p_mras.merge_requests_disable_committers_approval = project_dict["merge_requests_disable_committers_approval"]
+                            p_mras.save()
+                        if "require_password_to_approve" in project_dict:
+                            p_mras = project.approvals.get()
+                            p_mras.require_password_to_approve = project_dict["require_password_to_approve"]
+                            p_mras.save()
+                        # Protected branches
+                        if "protected_branches" in project_dict:
+                            for branch in project_dict["protected_branches"]:
+                                if any(project_branch.name == branch["name"] for project_branch in project.protectedbranches.list()):
+                                    p_branch = project.protectedbranches.get(branch["name"])
+                                    p_branch.delete()
+                                project.protectedbranches.create(
+                                    {
+                                        'name': branch["name"],
+                                        'push_access_level': branch["push_access_level"],
+                                        'merge_access_level': branch["merge_access_level"],
+                                        'merge_access_level': branch["merge_access_level"],
+                                        'code_owner_approval_required': branch["code_owner_approval_required"]
+                                    }
+                                )
+                            project.save()
                         # Protected tags
                         if "protected_tags" in project_dict:
                             for tag in project_dict["protected_tags"]:
-                                if not any(project_tag.name == tag for project_tag in project.protectedtags.list()):
-                                    project.protectedtags.create({'name': tag, 'create_access_level': '40'})
+                                if any(project_tag.name == tag["name"] for project_tag in project.protectedtags.list()):
+                                    p_tag = project.protectedtags.get(tag["name"])
+                                    p_tag.delete()
+                                project.protectedtags.create({'name': tag["name"], 'create_access_level': tag["create_access_level"]})
+                        # MR approval rules (should be done after branch protection reset)
+                        if "merge_request_approval_rules" in project_dict:
+                            # Empty list before setting
+                            for mrar in project.approvalrules.list():
+                                if mrar.name == 'All Members':
+                                    continue
+                                mrar.delete()
+                            for rule in project_dict["merge_request_approval_rules"]:
+
+                                g_ids = []
+                                for gr in rule["groups"]:
+                                    group_name = gr.split("/")[-1]
+                                    for group in gl.groups.list(search=group_name):
+                                        if group.full_path == gr:
+                                            g_ids.append(group.id)
+                                            logger.info("Found group ID: {id}, name: {group}".format(group=group.full_name, id=group.id))
+
+                                project.approvalrules.create(
+                                    {
+                                        'name': rule["name"],
+                                        'approvals_required': rule["approvals_required"],
+                                        'rule_type': 'regular',
+                                        'protected_branch_ids': [project.protectedbranches.get(rule["branch"]).id],
+                                        'group_ids': g_ids
+                                    }
+                                )
+                        # Runners
+                        if "specific_runners_enable" in project_dict:
+                            for runner_to_add in project_dict["specific_runners_enable"]:
+                                for runner in gl.runners.list():
+                                    if runner.description == runner_to_add:
+                                        if not any(added_runner.description == runner_to_add for added_runner in project.runners.list()):
+                                            project.runners.create({'runner_id': runner.id})
+                        # Protected envs
+                        if "protected_environments" in project_dict:
+                            for env in project_dict["protected_environments"]:
+                                # Create env first
+                                if not any(p_env.name == env["name"] for p_env in project.environments.list()):
+                                    project.environments.create({'name': env["name"]})
+                                # Protect with curl (python not yet supported)
+                                data = {
+                                    "name": env["name"],
+                                    "deploy_access_levels": [
+                                        {
+                                            "access_level": env["deploy_access_level"]
+                                        }
+                                    ]
+                                }
+                                script = textwrap.dedent(
+                                    """
+                                    curl --request POST \
+                                            --header "PRIVATE-TOKEN: {private_token}" \
+                                            --header "Content-Type: application/json" \
+                                            --data '{data}' \
+                                            "{gitlab_url}/api/v4/projects/{path_with_namespace_encoded}/protected_environments"
+                                    """
+                                ).format(
+                                    gitlab_url=projects_yaml_dict["gitlab"]["url"],
+                                    private_token=GL_ADMIN_PRIVATE_TOKEN,
+                                    path_with_namespace_encoded=project.path_with_namespace.replace("/", "%2F"),
+                                    data=json.dumps(data)
+                                )
+                                logger.info("Running bash script:")
+                                logger.info(script)
+                                subprocess.run(script, shell=True, universal_newlines=True, check=True, executable="/bin/bash")
                         # CI Variables
                         if "variables" in project_dict:
                             # We cannot update vars as key for update is not scope safe, so we delete first
@@ -176,8 +301,16 @@ if __name__ == "__main__":
                     logger.info(project)
                     logger.info("Project {project} deploy keys:".format(project=project_dict["path"]))
                     logger.info(project.keys.list())
+                    logger.info("Project {project} deploy tokens:".format(project=project_dict["path"]))
+                    logger.info(project.deploytokens.list())
+                    logger.info("Project {project} protected branches:".format(project=project_dict["path"]))
+                    logger.info(project.protectedbranches.list())
                     logger.info("Project {project} protected tags:".format(project=project_dict["path"]))
                     logger.info(project.protectedtags.list())
+                    logger.info("Project {project} runners:".format(project=project_dict["path"]))
+                    logger.info(project.runners.list())
+                    logger.info("Project {project} environments:".format(project=project_dict["path"]))
+                    logger.info(project.environments.list())
                     logger.info("Project {project} variables:".format(project=project_dict["path"]))
                     logger.info(project.variables.list())
                     for project_var in project.variables.list():
